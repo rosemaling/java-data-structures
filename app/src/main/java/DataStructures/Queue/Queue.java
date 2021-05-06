0ebfec9b3c7211d27397df116e23c6c138c99a15
Rose Brame <gillianbrame@gmail.com>
2021-05-06 11:13:56 -0500

    .remove passed

--- app/src/main/java/DataStructures/Queue/Queue.java
+++ app/src/main/java/DataStructures/Queue/Queue.java
@@ -19,4 +19,23 @@ public class Queue {
             current.next = addition;
         }
     }
+
+    public Object peek() {
+        return front.getData();
+    }
+
+    public Object remove(){
+        if (front.next != null) {
+            Object removal = front.getData();
+            front = front.next;
+            return removal;
+        } else if (front != null) {
+            Object removal = front.getData();
+            front = null;
+            return removal;
+        } else {
+            return null;
+        }
+    }
+
 }
--- app/src/test/java/Queue/QueueTest.java
+++ app/src/test/java/Queue/QueueTest.java
@@ -22,5 +22,45 @@ public class QueueTest {
         assertFalse(".add should add a new item to the back of the queue", actual);
     }
 
+    @Test
+    public void canPeekFrontNodeTest(){
+        Queue queue = new Queue();
+        queue.add("A SQL query walks into a bar and joins 2 tables.");
+        String expected = "A SQL query walks into a bar and joins 2 tables.";
+        String actual = (String) queue.peek();
+
+        assertEquals(".peek should return the first item in the queue", expected, actual);
+    }
+
+    @Test
+    public void peekDoesNotDeleteTest(){
+        Queue queue = new Queue();
+        queue.add("I have an inferiority complex, but it's not a very good one.");
+        queue.peek();
+        boolean actual = queue.isEmpty();
+
+        assertFalse(".peek should not remove the peeked item from the queue", actual);
+    }
+
+    @Test
+    public void removeDeletesTheFirstItemInTheQueue(){
+        Queue queue = new Queue();
+        queue.add("They all laughed when I said I wanted to be a comedienne -- well, they're not laughing now!");
+        queue.remove();
+        boolean actual = queue.isEmpty();
+
+        assertTrue(".remove should remove the first item in the queue", actual);
+    }
+
+    @Test
+    public void removeReturnsTheFirstItemInTheQueue(){
+        Queue queue = new Queue();
+        queue.add("A man walks into his house and is delighted when he discovers that someone has stolen all of his lamps.");
+        String expected = "A man walks into his house and is delighted when he discovers that someone has stolen all of his lamps.";
+        String actual = (String) queue.remove();
+
+        assertEquals(".remove should return the first item in the queue", expected, actual);
+    }
+
 
 }
