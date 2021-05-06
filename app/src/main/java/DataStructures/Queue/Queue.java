59c257991440b4a714d75541a25debada397554e
Rose Brame <gillianbrame@gmail.com>
2021-05-06 10:49:07 -0500

    Can add new node to queue

--- app/src/main/java/DataStructures/Queue/Queue.java
+++ app/src/main/java/DataStructures/Queue/Queue.java
@@ -6,4 +6,17 @@ public class Queue {
     public boolean isEmpty() {
         return front == null;
     }
+
+    public void add(Object data){
+        Node addition = new Node(data);
+        if (isEmpty()){
+            front = addition;
+        } else {
+            Node current = front;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = addition;
+        }
+    }
 }
--- app/src/test/java/Queue/QueueTest.java
+++ app/src/test/java/Queue/QueueTest.java
@@ -12,4 +12,15 @@ public class QueueTest {
 
         assertTrue(".isEmpty should return true on a brand-new queue", actual);
     }
+
+    @Test
+    public void canAddNewNodeTest(){
+        Queue queue = new Queue();
+        queue.add("I have the heart of a lion and a lifetime ban from the zoo.");
+        boolean actual = queue.isEmpty();
+
+        assertFalse(".add should add a new item to the back of the queue", actual);
+    }
+
+
 }
